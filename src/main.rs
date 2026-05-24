b74682ca1edf1eaca80e4bdbe77368b976088cdc
wolfy <wolfy@shitwolfymakes.com>
2026-05-24 15:54:55 -0500

    replace hello world with health check

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!\n", &name)
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     HttpServer::new( || {
             App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
     .run()
