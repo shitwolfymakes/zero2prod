5ad39bb9540ac13fd15b888aaea2ef770f30afaf
wolfy <wolfy@shitwolfymakes.com>
2026-07-11 17:45:51 -0500

    Update init_db.sh

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,20 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "Error: psql is not installed"
+    echo >&2 "sudo apt install postgresql-common postgres-client-17"
+    exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed"
+    echo >&2 "Use:"
+    echo >&2 "    cargo install --version="~0.7" sqlx-cli --no-default-features --features rustls,postgres"
+    echo >&2 "to install it."
+    exit 1
+fi
+
 # Check if a custom user has been set, otherwise default to 'postgres'
 DB_USER="${POSTGRES_USER:=postgres}"
 # Check if a custom password has been set, otherwise default to 'password'
@@ -14,9 +28,23 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 docker run \
-    -e DB_USER=${DB_USER} \
-    -e DB_PASSWORD=${DB_PASSWORD} \
-    -e DB_NAME=${DB_NAME} \
+    --name postgres \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
     -p "${DB_PORT}":5432 \
     -d postgres \
     postgres -N 1000
+
+# ping postgres until it's ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h ${DB_HOST} -U ${DB_USER} -p ${DB_PORT} -d "postgres" -c '\q'; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}"
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create
