9ea141601d9064ad4ce9807a992167c78f0bffe2
wolfy <wolfy@shitwolfymakes.com>
2026-07-11 14:32:45 -0500

    fix the function definition

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,14 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+
 // Let's start simple: we always return a 200 OK
-async fn subscribe() -> HttpResponse {
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
