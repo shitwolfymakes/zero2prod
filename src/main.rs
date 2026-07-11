e55c4b5ca101da4b843c0e3337e01b354e8775ab
wolfy <wolfy@shitwolfymakes.com>
2026-07-11 13:56:15 -0500

    server now serves main app on 8000

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ use std::net::TcpListener;
 async fn main() -> Result<(), std::io::Error> {
     // Bubble up the io:Error if we failed to bind the address
     // Otherwise call await on our Server
-    let listener = TcpListener::bind("127.0.0.1:0")
+    let listener = TcpListener::bind("127.0.0.1:8000")
         .expect("Failed to bind to random port");
     // We retrieve the port assigned to us by the OS
-    //let port = listener.local_addr().unwrap().port();
+    // let port = listener.local_addr().unwrap().port();
+    println!("http://127.0.0.1:{}", 8000);
     run(listener)?.await
 }
