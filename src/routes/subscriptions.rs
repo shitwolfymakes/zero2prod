1d25db2cebcaeb60903e78f14d09910aa8f00ebd
wolfy <wolfy@shitwolfymakes.com>
2026-07-13 13:32:34 -0500

    swap in tracing span

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -16,13 +16,25 @@ pub async fn subscribe(
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding {} {} as a new subscriber",
-        request_id,
-        form.email,
-        form.name,
+    // Spans, like logs, have an associated level
+    // `info_span` creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,                        // % tells `tracing` to use their Display impl for logging purposes
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
     );
-    log::info!("request_id {} - Saving new subscriber details in the database", request_id);
+    // using `enter` in an async function is a recipe for disaster!
+    // Bear with me for now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+    // tracing::info!(
+    //     "request_id {} - Adding {} {} as a new subscriber",
+    //     request_id,
+    //     form.email,
+    //     form.name,
+    // );
+    tracing::info!("request_id {} - Saving new subscriber details in the database", request_id);
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions(id, email, name, subscribed_at)
@@ -37,12 +49,14 @@ pub async fn subscribe(
     .await
     {
         Ok(_) => {
-            log::info!("request_id {} - New subscriber details have been saved", request_id);
+            tracing::info!("request_id {} - New subscriber details have been saved", request_id);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            log::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
+            tracing::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
+    // `_request_span_guard` is dropped at the end of `subscribe`
+    // that's where we "exit" the span
 }
