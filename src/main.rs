9be93e9063db8f76e839d906a8442466783a717d
wolfy <wolfy@shitwolfymakes.com>
2026-07-13 13:22:48 -0500

    observability pt 1

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 //! src/main.rs
 use std::net::TcpListener;
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
+
     let connection_pool = PgPool::connect(
         &configuration.database.connection_string()
     )
@@ -16,6 +23,6 @@ async fn main() -> Result<(), std::io::Error> {
     // we have removed the hardcoded 8000, it's now coming from our settings
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(&address)?;
-    println!("http://{}:{}", &address, configuration.application_port);
+    println!("http://{}", &address);
     run(listener, connection_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 //! src/startup.rs
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use crate::routes::{health_check, subscribe};
@@ -17,6 +18,7 @@ pub fn run(
     // capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+        .wrap(Logger::default())
         .route("health_check", web::get().to(health_check))
         .route("/subscriptions", web::post().to(subscribe))
         // Register the connection as part of the application state
