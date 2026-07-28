32321434cd24a461c8b3caefcf59d3395776256c
wolfy <wolfy@shitwolfymakes.com>
2026-07-28 13:05:49 -0500

    add SubscriberName

---
+++ src/domain.rs
@@ -0,0 +1,35 @@
+//! src/domain.rs
+use unicode_segmentation::UnicodeSegmentation;
+pub struct SubscriberName(String);
+
+impl SubscriberName {
+    /// Returns an instance of `SubscriberName` if the input satisfies all
+    /// our validation constraints on subscriber names.
+    /// It panics otherwise.
+    pub fn parse(s: String) -> SubscriberName {
+        // `.trim()` returns a view over the input `s` without trailing
+        // whitespace-like characters.
+        // `.is_empty()` checks if the view contains any character.
+        let is_empty_or_whitespace = s.trim().is_empty();
+
+        // A grapheme is defined by the Unicode standard as a "user-percieved"
+        // character.
+        // `graphemes` returns an iterator over the graphemes in the input `s`.
+        // `true` specifies that we want to use the extended grapheme definition set,
+        // the recommended one
+        let is_too_long = s.graphemes(true).count() > 256;
+
+        // Iterate over all characters in the input `s` to check if any of them
+        // matches one of the characters in the forbidden array
+        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let contains_forbidden_characters = s
+            .chars()
+            .any(|g| forbidden_characters.contains(&g));
+
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
+            panic!("{} is not a vailid subscriber name.", s)
+        } else {
+            Self(s)
+        }
+    }
+}
